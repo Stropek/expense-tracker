83798ff1facb180d45bcd4407c9cc5ef07aae8da
przemo <curzytekp@gmail.com>
2017-12-31 00:27:30 -0500

    set background color on categories list items

--- app/src/main/java/com/pscurzytek/expensetracker/CategoryAdapter.kt
+++ app/src/main/java/com/pscurzytek/expensetracker/CategoryAdapter.kt
@@ -2,10 +2,12 @@ package com.pscurzytek.expensetracker
 
 import android.content.Context
 import android.database.Cursor
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.pscurzytek.expensetracker.data.ExpenseContract
 
@@ -17,16 +19,23 @@ class CategoryAdapter(context: Context): RecyclerView.Adapter<CategoryAdapter.Ca
     private var mContext = context
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        val idIndex = mCursor!!.getColumnIndex(ExpenseContract.ExpenseCategory.ID)
-        val nameIndex = mCursor!!.getColumnIndex(ExpenseContract.ExpenseCategory.COLUMN_NAME)
+        val cursor = mCursor
 
-        mCursor?.moveToPosition(position)
+        if (cursor != null) {
+            val idIndex = cursor.getColumnIndex(ExpenseContract.ExpenseCategory.ID)
+            val nameIndex = cursor.getColumnIndex(ExpenseContract.ExpenseCategory.COLUMN_NAME)
+            val typeIndex = cursor.getColumnIndex(ExpenseContract.ExpenseCategory.COLUMN_TYPE)
 
-        val id = mCursor!!.getInt(idIndex)
-        val name = mCursor!!.getString(nameIndex)
+            cursor.moveToPosition(position)
 
-        holder.itemView.tag = id
-        holder.tvCategoryName.text = name
+            val id = cursor.getInt(idIndex)
+            val name = cursor.getString(nameIndex)
+            val type = cursor.getString(typeIndex)
+
+            holder.itemView.tag = id
+            holder.tvCategoryName.text = name
+            (holder.tvCategoryType.parent as LinearLayout).setBackgroundColor(if (type.toLowerCase() == "income") Color.GREEN else Color.RED)
+        }
     }
 
     override fun getItemCount(): Int {
